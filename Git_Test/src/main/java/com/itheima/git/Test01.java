3150d648f98938d8041611dc9167487758ec58e6
zhangsan <zhangsan@itcast.com>
2019-06-25 13:19:29 +0800

    IDE添加本地工程到git--修改

--- Git_Test/src/main/java/com/itheima/git/Test01.java
+++ Git_Test/src/main/java/com/itheima/git/Test01.java
@@ -4,4 +4,11 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Git use test...");
     }
+
+
+    public void get(){
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
 }
